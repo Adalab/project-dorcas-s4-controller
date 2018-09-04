1ebe15b9c5bea0d721bb6dc643797f45422c345e
Claudia <cl.calerolopez@gmail.com>
2018-09-04 20:25:34 +0200

    simple layout MapComponent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Map from './components/Map';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-
+          <Map />
         </header>
       </div>
     );
---
+++ src/components/Map.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import map from '../images/exampleMap.jpg';
+import direction from '../images/exampleDirection.jpg';
+
+class Map extends React.Component {
+  render() {
+    return (
+      <div className="establishmentDetails">
+        <section className="sectionList">
+        </section>
+        <section className="sectionDetails">
+          <img className="exampleMap" src={map} alt="exampleMap" />
+          <img className="exampleDirection" src={direction} alt="exampleDirection" />
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Map;
