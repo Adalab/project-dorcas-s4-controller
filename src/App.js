e4eb1d76c03a16664192726e8878132b837ba278
Fanyfa <estefania.salgado@gmail.com>
2018-09-04 17:42:27 +0200

    building login current value ok

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
 import Login from './components/Login';
-
+import api_token from './data/api_token.json';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+  }
+
+  launchLogin(email, password) {
+    console.log(email);
+    console.log(password);
+    api_token.requests[2].data[0].value
+    api_token.requests[2].data[1].value
+  }
+
   render() {
     return (
       <div className="App">
-        <Login/>
+        <Login launchLogin={this.launchLogin}/>
       </div>
     );
   }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,21 @@
 import React from 'react';
 
 class Login extends React.Component {
+  inputEmail = React.createRef();
+  inputPassword = React.createRef();
+
+  constructor(props) {
+    super(props);
+    this.onClickButton = this.onClickButton.bind(this);
+  }
+
+  onClickButton() {
+    this.props.launchLogin(
+      this.inputEmail.current.value,
+      this.inputPassword.current.value
+    );
+  }
+
   render() {
     return (
       <div>
@@ -10,11 +25,11 @@ class Login extends React.Component {
         </section>
         <section>
           <label htmlFor="">Correo electrónico*</label>
-          <input type="text"/>
+          <input ref={this.inputEmail} type="text"/>
           <label htmlFor="">Contraseña</label>
-          <input type="text"/>
+          <input ref={this.inputPassword} type="text"/>
           <p>¿Has olvidado tu contraseña?</p>
-          <button>Acceder</button>
+          <button onClick={this.onClickButton}>Acceder</button>
           <p>¿No tienes una cuenta? Regístrate</p>
         </section>
       </div>
