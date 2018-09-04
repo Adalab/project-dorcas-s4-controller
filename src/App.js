102709b6f923effe231f1d8af7e0cdcdd3b097c6
SandraCGarcia <sandra.gcanete@gmail.com>
2018-09-04 22:25:59 +0200

    empty

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
-import React, { Component } from 'react';
-import './App.css';
-import Login from './components/Login';
-import api_token from './data/api_token.json';
+import React, { Component } from "react";
+import "./App.css";
+import Login from "./components/Login";
+import api_token from "./data/api_token.json";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    
-    this.listBar=this.listBar.bind(this);
+
+    this.listBar = this.listBar.bind(this);
   }
 
   // componentDidMount() {
   //   listBar();
   // }
-// aquí se pasan los valores de los input por 
-//los parámetros que hemos enviado desde login.js
+  // aquí se pasan los valores de los input por
+  //los parámetros que hemos enviado desde login.js
   launchLogin(email, password) {
-    const apiUserName= api_token.requests[2].data[0].value;
-    const apiPassWord=api_token.requests[2].data[1].value;
-   if(email===apiUserName && password===apiPassWord){
-    listBar();
-   }else{
-     return alert("El usuario no está registrado");
-   }
+    const apiUserName = api_token.requests[2].data[0].value;
+    const apiPassWord = api_token.requests[2].data[1].value;
+    if (email === apiUserName && password === apiPassWord) {
+      // listBar();
+    } else {
+      return alert("El usuario no está registrado");
+    }
   }
-listBar(){
-//esperando como accerder al token
-}
+  listBar() {
+    //esperando como accerder al token
+  }
 
   render() {
     return (
       <div className="App">
-        <Login launchLogin={this.launchLogin}/>
+        <Login launchLogin={this.launchLogin} />
       </div>
     );
   }
