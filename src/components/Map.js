b56b347a97bd36d595b50820ad62685bd5c92f56
Claudia <cl.calerolopez@gmail.com>
2018-09-04 21:33:19 +0200

    layout logout

--- src/components/Map.js
+++ src/components/Map.js
@@ -5,14 +5,20 @@ import direction from '../images/exampleDirection.jpg';
 class Map extends React.Component {
   render() {
     return (
-      <div className="establishmentDetails">
-        <section className="sectionList">
-        </section>
-        <section className="sectionDetails">
-          <img className="exampleMap" src={map} alt="exampleMap" />
-          <img className="exampleDirection" src={direction} alt="exampleDirection" />
-        </section>
-      </div>
+      <React.Fragment>
+        <div className="logout">
+          <p className="logoutName">José L.   |</p>
+          <div className="logoutIcon"><i className="fas fa-sign-out-alt"></i></div>
+        </div>
+        <div className="establishments">
+          <section className="sectionList">
+          </section>
+          <section className="sectionDetails">
+            <img className="exampleMap" src={map} alt="exampleMap" />
+            <img className="exampleDirection" src={direction} alt="exampleDirection" />
+          </section>
+        </div>
+      </React.Fragment>
     );
   }
 }
