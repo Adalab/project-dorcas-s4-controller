7279958fe52620b9fd177414b3772ca47d1393f3
EsterGonzalezG <estergonzalezgalindo@gmail.com>
2018-09-06 08:51:09 +0200

    fetch list establishment

--- src/App.js
+++ src/App.js
@@ -12,43 +12,66 @@ class App extends Component {
     }
     this.launchLogin = this.launchLogin.bind(this);
     this.logout = this.logout.bind(this);
+    this.postEstablishments=this.postEstablishments.bind(this);
+    this.login=this.login.bind(this);
   }
 
-  // aquí se pasan los valores de los input por
-  //los parámetros que hemos enviado desde login.js
+
   logout() {
     localStorage.removeItem('token');
   }
-  launchLogin(email, password) {
+  postEstablishments(savedToken){
+    const establishments='https://ada-controller.deploy-cd.com/api/establishments';
+    fetch(establishments, {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Bearer ' + savedToken
+      }  
+    })
+    .then(response => response.json())
+    .then(response2 => console.log(response2));
 
+  }
+  login(email, password,){
+    //aqui hacer comprobacion si el usuario no tiene acceso, para mostrar pantalla que no tiene acceso
     const url = "https://ada-controller.deploy-cd.com/api/login_check";
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({ _username: email, _password: password }),
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+      }
+    })
+      .then(res => res.json())
+      .then(response => localStorage.setItem('token', JSON.stringify(response.token)));
+      
+      //hacer otra peticion para mostrar los establecimientos la 1 vez que hace login y no está el LS
+  }
+
+    // aquí se pasan los valores de los input por
+   //los parámetros que hemos enviado desde login.js
+  launchLogin(email, password) {
     const savedToken = JSON.parse(localStorage.getItem('token'));
     if (savedToken) {
       //se haría la petición al listado de bares y se pasaría a la página principal
+      this.postEstablishments(savedToken);
     } else {
       this.setState({
         email: email
       });
-      fetch(url, {
-        method: 'POST',
-        body: JSON.stringify({ _username: email, _password: password }),
-        headers: {
-          "Content-Type": "application/json; charset=utf-8",
-        }
-      })
-        .then(res => res.json())
-        .then(response => localStorage.setItem('token', JSON.stringify(response)));
+      this.login(email, password);
     }
   }
+  
 
   render() {
     //localStorage.removeItem('token');
     return (
       <div className="App">
         <Switch>
-          <Route exact path='/LayouPrincipal'
+          <Route exact path='/'
             render={() => <Login
-            // launchLogin={this.launchLogin}
+             launchLogin={this.launchLogin}
             />}
           />
           <Route path='/' render={(props) => < LayoutPrincipal email={this.state.email} match={props.match} />}
