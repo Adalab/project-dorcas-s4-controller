75398e1901d06ef8d7200ca1644d3839057f61a6
Sofía Rodríguez <sofiarincon.artandmath@gmail.com>
2018-09-06 11:32:28 +0200

    some states

--- src/App.js
+++ src/App.js
@@ -8,33 +8,36 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: ''
+      email: '',
+      name: '',
+      address: '',
+      affiliates: ''
     }
     this.launchLogin = this.launchLogin.bind(this);
     this.logout = this.logout.bind(this);
-    this.postEstablishments = this.postEstablishments.bind(this);
-    this.login = this.login.bind(this);
+    this.postEstablishments=this.postEstablishments.bind(this);
+    this.login=this.login.bind(this);
   }
+
 
   logout() {
     localStorage.removeItem('token');
   }
-  postEstablishments(savedToken) {
-    const establishments = 'https://ada-controller.deploy-cd.com/api/establishments';
+  postEstablishments(savedToken){
+    const establishments='https://ada-controller.deploy-cd.com/api/establishments';
     fetch(establishments, {
       method: 'GET',
       headers: {
         'Authorization': 'Bearer ' + savedToken
       }
     })
-      .then(response => response.json())
-      .then(response2 => console.log(response2));
-  }
+    .then(response => response.json())
+    .then(response2 => console.log(response2));
 
-  login(email, password) {
+  }
+  login(email, password,){
     //aqui hacer comprobacion si el usuario no tiene acceso, para mostrar pantalla que no tiene acceso
     const url = "https://ada-controller.deploy-cd.com/api/login_check";
-    const establishments = 'https://ada-controller.deploy-cd.com/api/establishments';
     fetch(url, {
       method: 'POST',
       body: JSON.stringify({ _username: email, _password: password }),
@@ -43,23 +46,13 @@ class App extends Component {
       }
     })
       .then(res => res.json())
-      .then(function(response) {
-        localStorage.setItem('token', JSON.stringify(response.token))
-    
-      fetch(establishments, {
-          method: 'GET',
-          headers: {
-            'Authorization': 'Bearer ' + response.token
-          }
-        })
-          .then(response1 => response1.json())
-          .then(response2 => console.log(response2));  
-  //   //hacer otra peticion para mostrar los establecimientos la 1 vez que hace login y no está el LS
-  });
-}
+      .then(response => localStorage.setItem('token', JSON.stringify(response.token)));
 
-  // aquí se pasan los valores de los input por
-  //los parámetros que hemos enviado desde login.js
+      //hacer otra peticion para mostrar los establecimientos la 1 vez que hace login y no está el LS
+  }
+
+    // aquí se pasan los valores de los input por
+   //los parámetros que hemos enviado desde login.js
   launchLogin(email, password) {
     const savedToken = JSON.parse(localStorage.getItem('token'));
     if (savedToken) {
@@ -74,13 +67,13 @@ class App extends Component {
   }
 
   render() {
-    // localStorage.removeItem('token');
+    //localStorage.removeItem('token');
     return (
       <div className="App">
         <Switch>
           <Route exact path='/'
             render={() => <Login
-              launchLogin={this.launchLogin}
+             launchLogin={this.launchLogin}
             />}
           />
           <Route path='/' render={(props) => < LayoutPrincipal email={this.state.email} match={props.match} />}
