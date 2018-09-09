f2e2867fb3d92b475e128a4caf593874a48610b2
Claudia <cl.calerolopez@gmail.com>
2018-09-09 17:47:51 +0200

    layout basic detail establishment

--- src/components/DetailBar.js
+++ src/components/DetailBar.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import map from '../images/exampleMap.jpg';
-import direction from '../images/exampleDirection.jpg';
 import '../stylesheet/map.css';
 import '../stylesheet/buttons.css';
+import '../stylesheet/detailBar.css';
 
 class Map extends React.Component {
   render() {
@@ -15,20 +15,21 @@ class Map extends React.Component {
             <section className="sectionDetails">
               <img className="exampleMap" src={map} alt="exampleMap" />
               {/* <img className="exampleDirection" src={direction} alt="exampleDirection" /> */}
-                  <div className="establishments__div__details">
+              <div className="establishments__div__details">
                 <div className="details__img" style={{ backgroundImage: `url()` }}>
-                  <img className="imgDetails" src="" alt="" />
+                  <img className="imgDetails" src="http://via.placeholder.com/230x230" alt="" />
                 </div>
                 <div className="details__data">
-                  <h2 className="titleDatos"></h2>
+                  <h2 className="data__title">{this.props.establishments.data[0].name}</h2>
                   <div>
-                    <p className="parDatos">Dirección:</p>
-                    <p className="data__directio"></p>
-                    <p className="data__meters"></p>
+                    <p className="data">Dirección</p>
+                    <p className="data__direction">{this.props.establishments.data[0].address}{this.props.establishments.data[0].postalcode}{this.props.establishments.data[0].province}</p>
+                    <p className="data__meters">A 543m</p>
                   </div>
+                  <p className="data__affiliates">{this.props.establishments.data[0].affiliates}</p>
                 </div>
               </div>
-            <div className="divBtb"><button className="btnReport"><i className="fas fa-clipboard-list"></i>Reportar</button></div>
+              <div className="divBtb"><button className="btnReport"><i className="fas fa-clipboard-list"></i>Reportar</button></div>
             </section>
           </div>
         </React.Fragment>
