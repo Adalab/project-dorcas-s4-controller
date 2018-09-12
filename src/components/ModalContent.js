91a9c4c152d0cd640954b1eadfa86832b94a772b
Fanyfa <estefania.salgado@gmail.com>
2018-09-12 13:33:45 +0200

    style modal

--- src/components/ModalContent.js
+++ src/components/ModalContent.js
@@ -1,4 +1,5 @@
 import React from "react";
+import  '../stylesheet/ModalContent.css';
 // import ReactDOM from 'react-dom';
 
 class ModalContent extends React.Component {
@@ -8,8 +9,10 @@ class ModalContent extends React.Component {
         <div className="modal__content">
           ¿Has disfrutado de la experiencia en nuestro establecimiento?
         </div>
-        <button className="modal__btn--yes">Sí</button>
-        <button className="modal__btn--no">No</button>
+        <div className="modal__btn">
+          <button className="modal__btn--yes">Sí</button>
+          <button className="modal__btn--no">No</button>
+        </div>
       </React.Fragment>
     );
   }
