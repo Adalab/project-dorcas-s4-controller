f86e3e8b1f72983fb66d8853a624ed0584a03664
Fanyfa <estefania.salgado@gmail.com>
2018-09-19 17:54:01 +0200

    map wirh ansers

--- src/components/ModalContent.js
+++ src/components/ModalContent.js
@@ -3,32 +3,7 @@ import '../stylesheet/ModalContent.css';
 
 class ModalContent extends React.Component {
 
-  // constructor(props) {
-  //   super(props);
-  //   this.saveAnswer=this.saveAnswer.bind(this);
-  // }
-  // saveAnswer(e){
-  //   this.props.setModalQuestionStage();
-  //   // this.buttonNo.current.value;
-  //   // this.buttonYes.current.value;
-  //   if(this.props.modalQuestionsStage===0){
-  //     if (e.currentTarget.getAttribute('id') === 'yes'){
-  //      return(<p>Sí</p>);
-  //     }else{
-  //       return(<p>No</p>);
-  //     }
-  //   }else{
-  //     if (e.currentTarget.getAttribute('id') === 'yes'){
-  //       return(<p>Sí</p>);
-  //     }else{
-  //       return(<p>No</p>);
-  //     }
-  //   }
-    
-  // }
-
   render() {
-    
     if (this.props.questions.length <= 0) {
       return null;
     } else {
@@ -40,10 +15,15 @@ class ModalContent extends React.Component {
             </header>
             <div className="modal__summary__content">
               <ol className="modal__summary__list">
-                <li className="modal__summary__list__item">{this.props.questions[0].questions[0].message}</li>
-                <p className="modal__summary__answers">{this.props.answers[0]}</p>
-                <li className="modal__summary__list__item">{this.props.questions[0].questions[1].message}</li>
-                <p className="modal__summary__answers">{this.props.answers[1]}</p>
+                {this.props.questions[0].questions.map((item, index) => {
+                  return (
+                    <React.Fragment>
+                      <li className="modal__summary__list__item">{item.message}</li>
+                      <p className="modal__summary__answers">
+                        {this.props.answers[index]}</p>
+                    </React.Fragment>
+                  )
+                })}
               </ol>
             </div>
             <button className="modal__button__send" onClick={this.props.sendSummary}>Envíar informe</button>
@@ -62,12 +42,12 @@ class ModalContent extends React.Component {
                 {this.props.questions[0].questions[this.props.modalQuestionsStage].message}
               </div>
               <div className="modal__btn">
-                {/* <input className={this.props.modalButtonYes}  id="yes" onClick={this.props.modalButtons} type="button" value="Sí"></input> */}
-                <button className={this.props.modalButtonYes}  id="yes" onClick={eventClick => {
-                  this.props.modalButtons(eventClick, 'Sí');}} >Sí</button>
-                {/* <input className={this.props.modalButtonNo}  id="no" onClick={this.props.modalButtons} type="button" value="No"></input> */}
-                <button className={this.props.modalButtonNo}  id="no" onClick={eventClick => {
-                  this.props.modalButtons(eventClick, 'No');}} >No</button>
+                <button className={this.props.modalButtonYes} id="yes" onClick={eventClick => {
+                  this.props.modalButtons(eventClick, 'Sí');
+                }} >Sí</button>
+                <button className={this.props.modalButtonNo} id="no" onClick={eventClick => {
+                  this.props.modalButtons(eventClick, 'No');
+                }} >No</button>
                 <button className="buttonArrow" onClick={this.props.setModalQuestionStage}><i className={this.props.nextButton}></i></button>
               </div>
             </div>
